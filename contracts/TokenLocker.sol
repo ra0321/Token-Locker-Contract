cb567d306a96cb74e7d13ca2c5a8d2c390f1e294
ra0321 <alphanetra0321@gmail.com>
2021-03-20 12:50:31 +0100

    Refactor smart contract name

--- contracts/TokenLocker.sol
+++ contracts/TokenLocker.sol
@@ -1,5 +1,91 @@
 pragma solidity ^0.7.6;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract TokenLocker {
-    constructor() {}
+    address public owner;
+    uint256 panicWithdrawalFee = 15;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only available to the contract owner.");
+        _;
+    }
+
+    event Hodl(address indexed hodler, address token, uint256 amount, uint256 unlockTime);
+
+    event PanicWithdraw(address indexed hodler, address token, uint256 amount, uint256 timediff);
+
+    event Withdrawal(address indexed hodler, address token, uint256 amount);
+
+    event FeesClaimed();
+
+    struct Hodler {
+        address hodlerAddress;
+        mapping(address => Token) tokens;
+    }
+
+    struct Token {
+        uint256 balance;
+        address tokenAddress;
+        uint256 unlockTime;
+    }
+
+    mapping(address => Hodler) public hodlers;
+
+    function hodlDeposit(
+        address token,
+        uint256 amount,
+        uint256 unlockTime
+    ) public {
+        Hodler storage hodler = hodlers[msg.sender];
+        hodler.hodlerAddress = msg.sender;
+
+        hodlers[msg.sender].tokens[token] = Token(amount, token, unlockTime);
+
+        ERC20(token).transferFrom(msg.sender, address(this), amount);
+        Hodl(msg.sender, token, amount, unlockTime);
+    }
+
+    function withdraw(address token) public {
+        Hodler storage hodler = hodlers[msg.sender];
+        require(block.timestamp > hodler.tokens[token].unlockTime, "Unlock time not reached yet.");
+
+        uint256 amount = hodler.tokens[token].balance;
+        hodler.tokens[token].balance = 0;
+        ERC20(token).transfer(msg.sender, amount);
+
+        Withdrawal(msg.sender, token, amount);
+    }
+
+    function panicWithdraw(address token) public {
+        Hodler storage hodler = hodlers[msg.sender];
+        hodler.hodlerAddress = msg.sender;
+
+        uint256 feeAmount = (hodler.tokens[token].balance / 100) * panicWithdrawalFee;
+        uint256 withdrawalAmount = hodler.tokens[token].balance - feeAmount;
+
+        hodler.tokens[token].balance = 0;
+        //Transfers fees to the contract administrator/owner
+        hodlers[address(owner)].tokens[token].balance = feeAmount;
+
+        ERC20(token).transfer(msg.sender, withdrawalAmount);
+
+        PanicWithdraw(msg.sender, token, withdrawalAmount, hodler.tokens[token].unlockTime - block.timestamp);
+    }
+
+    function claimFees(address[] memory tokenList) public onlyOwner {
+        for (uint256 i = 0; i < tokenList.length; i++) {
+            uint256 amount = hodlers[owner].tokens[tokenList[i]].balance;
+            if (amount > 0) {
+                ERC20(tokenList[i]).transfer(owner, amount);
+                hodlers[owner].tokens[tokenList[i]].balance = 0;
+            }
+        }
+        FeesClaimed();
+    }
 }
--- test/TokenLocker.ts
+++ test/TokenLocker.ts
@@ -1,13 +1,13 @@
 import hre from "hardhat";
 import { Artifact } from "hardhat/types";
 import { BasicToken } from "../typechain";
-import { HodlTokens } from "../typechain";
+import { TokenLocker } from "../typechain";
 import { expect } from "chai";
 import { MockProvider } from "ethereum-waffle";
 
 const { deployContract } = hre.waffle;
 
-describe("Smart Contract operations tests", function () {
+describe("Smart Contract operations tests", function() {
   const [wallet, otherWallet] = new MockProvider({
     ganacheOptions: {
       gasLimit: 10000000,
@@ -15,50 +15,51 @@ describe("Smart Contract operations tests", function () {
     },
   }).getWallets();
   let token: BasicToken;
-  let contract: HodlTokens;
+  let contract: TokenLocker;
 
-  describe("HodlTokens", function () {
+  describe("TokenLocker", function() {
 
-    beforeEach(async function () {
-      const hodlTokensArtifact: Artifact = await hre.artifacts.readArtifact("HodlTokens");
-      contract = <HodlTokens>await deployContract(wallet, hodlTokensArtifact, [wallet.address]);
+    beforeEach(async function() {
+      const hodlTokensArtifact: Artifact = await hre.artifacts.readArtifact("TokenLocker");
+      contract = <TokenLocker>await deployContract(wallet, hodlTokensArtifact, [wallet.address]);
       const tokenArtifact: Artifact = await hre.artifacts.readArtifact("BasicToken");
       token = <BasicToken>await deployContract(wallet, tokenArtifact, [1000]);
     });
 
-    it("should deposit tokens from wallet to the contract", async function () {
+    it("should deposit tokens from wallet to the contract", async function() {
       await token.approve(contract.address, 10);
       await contract.connect(wallet).hodlDeposit(token.address, 10, 2020);
       expect(await token.balanceOf(contract.address)).to.equal(10);
       expect(await token.balanceOf(wallet.address)).to.equal(990);
     });
 
-    it("should withdraw tokens from contract to the wallet", async function () {
+    it("should withdraw tokens from contract to the wallet", async function() {
       await token.approve(contract.address, 10);
       await contract.connect(wallet).hodlDeposit(token.address, 10, 2020);
       await contract.connect(wallet).withdraw(token.address);
       expect(await token.balanceOf(wallet.address)).to.equal(1000);
     });
 
-    it("should apply fee and leave a percent of tokens on the contract on panicWithdraw", async function () {
+    it("should apply fee and leave a percent of tokens on the contract on panicWithdraw", async function() {
       await token.approve(contract.address, 100);
       await contract.connect(wallet).hodlDeposit(token.address, 100, 2020);
       await contract.connect(wallet).panicWithdraw(token.address);
       expect(await token.balanceOf(contract.address)).to.equal(15);
     });
 
-    it("should fail withdrawing tokens because of the time limit", async function () {
-      const dateInFuture = Math.floor(Date.now() / 1000) + 6000;
+    it("should fail withdrawing tokens because of the time limit", async function() {
+      const dateNow = Math.floor(Date.now() / 1000);
+      const dateInFuture = dateNow + 6000;
       await token.approve(contract.address, 10);
       await contract.connect(wallet).hodlDeposit(token.address, 10, dateInFuture);
-      await expect( contract.connect(wallet).withdraw(token.address)).to.be.reverted
+      await expect(contract.connect(wallet).withdraw(token.address)).to.be.reverted;
     });
 
-    it("should fail because it's not the owner", async function () {
-      await expect( contract.connect(otherWallet).claimFees([])).to.be.reverted
+    it("should fail because it's not the owner", async function() {
+      await expect(contract.connect(otherWallet).claimFees([])).to.be.reverted;
     });
 
-    it("should transfer leftover token fees to the original wallet", async function () {
+    it("should transfer leftover token fees to the original wallet", async function() {
       await token.approve(contract.address, 200);
       await contract.connect(wallet).hodlDeposit(token.address, 200, 2020);
       await contract.connect(wallet).panicWithdraw(token.address);
